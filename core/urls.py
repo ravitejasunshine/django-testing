ef17454d1dd57129f8ca104fcfed9df522c06d71
ravitejasunshine <ravitejag1995@gmail.com>
2022-06-29 11:07:31 +0530

    updated the views,urls,settings

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index),
+    path('Home/', views.Home),
+    path('Bye/', views.GoodBye),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Welcome to Django world")
+def Home(request):
+    return HttpResponse("<h1>Welcome to Home page</h1>") 
+def GoodBye(request):
+    return HttpResponse("<h1 style='color:red;font-family:tohama;font-size:40px;text-decoration:underline'>Thanks for visiting soon</h1>") 
+          
